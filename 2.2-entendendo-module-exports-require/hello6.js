9c1d77781ec97809d84aead7752fefbaaae85065
alcivanio <alcivaniosilva@gmail.com>
2016-11-16 20:12:01 -0300

    Metodo de importacao - require - do edit

---
+++ 2.2-entendendo-module-exports-require/hello6.js
@@ -0,0 +1,3 @@
+var libm = require('./matematica');
+console.log("Soma: " + libm.soma(2,2));
+console.log("Subtração: " + libm.subtracao(2,5));
---
+++ 2.2-entendendo-module-exports-require/matematica.js
@@ -0,0 +1,7 @@
+// matematica.js
+const mat = {
+  soma : (a,b) => a + b,
+  subtracao : (a,b) => a - b
+};
+// aqui estamos publicando o conteúdo (ou parte do conteúdo) deste módulo
+module.exports = mat;
